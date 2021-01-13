0e30c3289699adc508381196dafd32a85114d039
Queje <jeremyquerne@hotmail.fr>
2021-01-13 14:57:39 +0100

    adding another algorithm, exo 1, 3, 4 and 6 done

--- sorting.js
+++ sorting.js
@@ -10,7 +10,7 @@ class Sorting {
         this.turnCount = turnCount
     }
 
-    twoSum(array, target) {
+    twoSumExo1(array, target) {
         let result = [];
         for (let i = 0; i < array.length-1; i++) {
             for (let j = i+1; j < array.length; j++) {
@@ -27,18 +27,31 @@ class Sorting {
         }
     };
 
-    skyScraper1(array) {
-        const result =[]
-        if (array.length == 0) {
-            return result
-        } else if (array.length > 0) {
-            result.push(Math.max(...array))
-        }
-        console.log(result)
-        return this.skyScraper1(array.slice(array.lastIndexOf(Math.max(...array))+1,array.length))
+    skyScraperExo2(array) {
+
     };
 
-    skyScraper2(array) {
+    twoSumExo3(array, sum) {
+        let storageHash = {}
+        let nums = []
+        for(let i in array){
+            let addend = sum - array[i]
+            
+            if (addend in storageHash){
+                nums.push([addend, array[i]])
+            }
+            storageHash[array[i]] = i
+        }
+        if(nums.length > 0) {
+            console.log(nums)
+            return true
+        } else {
+            return false
+        }
+    };
+
+
+    skyScraperExo4(array) {
         let result = []
         result.push(array[array.length-1])
         for(let i=array.length; i > 1 ; i--){
@@ -49,18 +62,36 @@ class Sorting {
         return result.length
     };
 
+    twoSumExo5(array, sum){
+        
+    };
+
+    skyScraperExo6(array) {
+        const result =[]
+        if (array.length == 0) {
+            return result
+        } else if (array.length > 0) {
+            result.push(Math.max(...array))
+        }
+        console.log(result)
+        return this.skyScraperExo6(array.slice(array.lastIndexOf(Math.max(...array))+1,array.length))
+    };
+
 }
 
 function Apply() {
     const exercice = new Sorting();
     console.log(`sujet 1: exercice1: y a t'il 2 nombres dans le tableaux dont la somme fait ${value} ?`);
-    console.log(exercice.twoSum(list, value));
+    console.log(exercice.twoSumExo1(list, value));
 
-    console.log(`sujet 2: exercice1: Combien d'immeubles ayant la vue sur le soleil (skyline avec une seule boucle?) ? ${list2}`);
-    console.log(exercice.skyScraper1(list2));
+    console.log(`sujet 1: exercice3: y a t'il 2 nombres dans le tableaux dont la somme fait ${value} ?`);
+    console.log(exercice.twoSumExo3(list, value));
     
-    console.log(`sujet 2: exercice6: Combien d'immeubles ayant la vue sur le soleil (skyline avec une seule boucle?) ? ${list2}`);
-    console.log(exercice.skyScraper2(list2));
+    console.log(`sujet 2: exercice4: Combien d'immeubles ayant la vue sur le soleil (skyline avec une seule boucle?) ? ${list2}`);
+    console.log(exercice.skyScraperExo4(list2));
+
+    console.log(`sujet 2: exercice6: Combien d'immeubles ayant la vue sur le soleil (skyline avec une recursion) ? ${list2}`);
+    console.log(exercice.skyScraperExo6(list2));
 };
 
 Apply();
